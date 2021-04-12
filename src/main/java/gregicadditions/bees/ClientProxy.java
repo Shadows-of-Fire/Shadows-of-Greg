0e57ad0640a1edc96a728fdfc55058c1fdb36b68
DStrand1 <dane@strandboge.com>
2021-04-12 00:15:15 -0500

    Fix Comb recipes, restructure code

--- src/main/java/gregicadditions/bees/ClientProxy.java
+++ src/main/java/gregicadditions/bees/ClientProxy.java
@@ -19,16 +19,11 @@ import static gregicadditions.GregicAdditions.isForestryBeesDisabled;
 
 @Mod.EventBusSubscriber(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
-	@Override
-	public void preInit() {
-		super.preInit();
-	}
 
 	@Override
 	public void postInit() {
 		ItemColors itemColors = Minecraft.getMinecraft().getItemColors();
 		itemColors.registerItemColorHandler(ColoredItemItemColor.INSTANCE, GTCombs.combItem);
-		super.postInit();
 	}
 
 	@SubscribeEvent
