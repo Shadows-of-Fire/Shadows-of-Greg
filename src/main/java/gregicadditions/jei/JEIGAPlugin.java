4bda0dfa256c5bfbcb1044563dfd41fbfa8220d4
Exa <11907282+Exaxxion@users.noreply.github.com>
2026-07-16 01:11:13 -0700

    Merge SoG's separate JEI category into GTNE's
    Basically everything implemented for SoG's multiblock info category is
    redundant, so just add the multis to GTNE's existing JEI category.

--- src/main/java/gregicadditions/jei/JEIGAPlugin.java
+++ src/main/java/gregicadditions/jei/JEIGAPlugin.java
@@ -1,10 +1,10 @@
 package gregicadditions.jei;
 
-import gregtech.integration.jei.multiblock.MultiblockInfoPage;
+import gregtech.integration.jei.multiblock.MultiblockInfoCategory;
+import gregtech.integration.jei.multiblock.MultiblockInfoRecipeWrapper;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
-import mezz.jei.api.ingredients.VanillaTypes;
 import mezz.jei.api.recipe.IRecipeCategoryRegistration;
 
 import static gregicadditions.item.GAMultiblockCasing.CasingType.*;
@@ -14,25 +14,23 @@ public class JEIGAPlugin implements IModPlugin {
 
 	@Override
 	public void registerCategories(IRecipeCategoryRegistration registry) {
-		registry.addRecipeCategories(new GAMultiblockInfoCategory(registry.getJeiHelpers()));
+		injectMultiblocks();
+	}
+
+	private void injectMultiblocks() {
+		var map = MultiblockInfoCategory.multiblockRecipes;
+		map.put("assembly_line", new MultiblockInfoRecipeWrapper(new AssemblyLineInfo()));
+		map.put("fusion_reactor_1", new MultiblockInfoRecipeWrapper(new FusionReactor1Info()));
+		map.put("fusion_reactor_2", new MultiblockInfoRecipeWrapper(new FusionReactor2Info()));
+		map.put("fusion_reactor_3", new MultiblockInfoRecipeWrapper(new FusionReactor3Info()));
+		map.put("processing_array", new MultiblockInfoRecipeWrapper(new ProcessingArrayInfo()));
 	}
 
 	@Override
 	public void register(IModRegistry registry) {
-		GAMultiblockInfoCategory.registerRecipes(registry);
 		// Workaround - hide the Coke Oven blocks until we can remove them without ID shift
 		registry.getJeiHelpers()
 		        .getIngredientBlacklist()
 		        .addIngredientToBlacklist(COKE_OVEN_BRICKS.getStack());
-
-
-		//Multiblock info page registration
-		GAMultiblockInfoCategory.multiblockRecipes.values().forEach(v -> {
-			MultiblockInfoPage infoPage = v.getInfoPage();
-			registry.addIngredientInfo(infoPage.getController().getStackForm(),
-					VanillaTypes.ITEM,
-					infoPage.getDescription());
-		});
-
 	}
 }
